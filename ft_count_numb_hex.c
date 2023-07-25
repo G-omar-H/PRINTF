9753a6cae75a637fce090b374cf2051130f89d2f
G-omar-H <omarghazi12@gmail.com>
2023-07-25 13:28:34 -0700

    updates

---
+++ ft_count_numb_hex.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * ft_count_numb - Counts the number of digits in a long integer,
+ * including the sign
+ * @n: The long integer to count the digits of
+ *
+ * Return: The number of digits in the long integer
+ */
+int	ft_count_numb(long int n)
+{
+	int	i;
+
+	i = 1;
+	while (1)
+	{
+		if (n < 0)
+		{
+			i++;
+			n *= -1;
+		}
+		n = n / 10;
+		if (n)
+			i++;
+		else
+			break;
+	}
+	return (i);
+}
+
+/**
+ * count_numb_hex - Counts the number of digits in an-
+ * unsigned long integer (in hexadecimal representation)
+ * @n: The unsigned long integer to count the digits of
+ *
+ * Return: The number of digits in the unsigned long integer in
+ * hexadecimal representation
+ */
+int	count_numb_hex(unsigned long int n)
+{
+	int	i;
+
+	i = 0;
+	if (n == 0)
+		i = 1;
+	while (n)
+	{
+		n /= 16;
+		i++;
+	}
+	return (i);
+}
